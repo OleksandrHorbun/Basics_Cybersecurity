c40678d741a85ef77588b994aeb470d09768f002
OleksandrHorbun <90098607+OleksandrHorbun@users.noreply.github.com>
2021-12-22 05:16:42 +0200

    Done Practice8

---
+++ Lab8/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lab8
+{
+    class Program
+    {
+
+        private readonly static string CspContainerName = "RsaContainer";
+        public static void AssignNewKey(string publicKeyPath)
+        {
+            CspParameters cspParameters = new CspParameters(1)
+            {
+                KeyContainerName = CspContainerName,
+
+                ProviderName = "Microsoft Strong Cryptographic Provider"
+            };
+            var rsa = new RSACryptoServiceProvider(cspParameters)
+            {
+                PersistKeyInCsp = true
+            };
+            File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+        }
+        public static byte[] DecryptData(byte[] dataToDecrypt)
+        {
+            byte[] plainBytes;
+            var cspParams = new CspParameters
+            {
+                KeyContainerName = CspContainerName,
+
+            };
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                rsa.PersistKeyInCsp = true;
+                plainBytes = rsa.Decrypt(dataToDecrypt, false);
+            }
+            return plainBytes;
+        }
+        public static byte[] EncryptData(string publicKeyPath, byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+                cipherbytes = rsa.Encrypt(dataToEncrypt, false);
+            }
+            return cipherbytes;
+        }
+        static void Main(string[] args)
+        {
+            string toEncrypt = Console.ReadLine();
+
+            //string NamePublicKey = "Dolmatova.xml";
+            string NamePublicKey = "HorbunOleksandr.xml";
+
+            //AssignNewKey("Dolmatova.xml");
+            AssignNewKey("HorbunOleksandr.xml");
+            
+
+
+            var encrypted = EncryptData(NamePublicKey, Encoding.Unicode.GetBytes(toEncrypt));
+            File.WriteAllBytes("HorbunOleksandr.dat", encrypted);
+            Console.WriteLine("Message = " + toEncrypt);
+
+            //string NameForMessageToDecrypt = "Dolmatova.dat";
+            string NameForMessageToDecrypt = "HorbunOleksandr.dat";
+
+            var encryptedmessage =  File.ReadAllBytes(NameForMessageToDecrypt);
+            var decrypted = DecryptData(encryptedmessage);
+            Console.WriteLine("Decrypted message = " + Encoding.Unicode.GetString(decrypted));
+
+            Console.ReadKey();
+        }
+    }
+}
