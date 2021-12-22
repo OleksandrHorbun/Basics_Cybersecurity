da58822211ba0ee1bf86b5c59be7dd9d138a3ace
OleksandrHorbun <90098607+OleksandrHorbun@users.noreply.github.com>
2021-12-23 00:31:08 +0200

    For Exam

--- Exam/obj/Debug/netcoreapp3.1/Exam.AssemblyInfo.cs
+++ Exam/obj/Debug/netcoreapp3.1/Exam.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("lab9")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("lab9")]
-[assembly: System.Reflection.AssemblyTitleAttribute("lab9")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exam")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
