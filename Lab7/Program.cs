d7adcb03eec49a3a5cf54c606e5cc2685a621db2
OleksandrHorbun <90098607+OleksandrHorbun@users.noreply.github.com>
2021-12-22 02:32:52 +0200

    Done Practice7

---
+++ Lab7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lab7_1
+{
+    public class Program
+    {
+      private RSAParameters _publicKey;
+        private RSAParameters _privateKey;
+
+        public void AssignNewKey()
+        {
+            using(var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                _publicKey = rsa.ExportParameters(false);
+                _privateKey = rsa.ExportParameters(true);
+            }
+        }
+        public byte[] EncryptData(byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(_publicKey);
+                cipherbytes = rsa.Encrypt(dataToEncrypt, true);
+            }
+            return cipherbytes;
+        }
+        public byte[] DecryptData(byte[] dataToEncrypt)
+        {
+            byte[] plain;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(_privateKey);
+                plain = rsa.Decrypt(dataToEncrypt, true);
+            }
+            return plain;
+        }
+
+        static void Main(string[] args)
+        {
+            var rsaParams = new Program();
+            const string toEncrypt = "Something to encrypt...";
+            rsaParams.AssignNewKey();
+            var encrypted = rsaParams.EncryptData(Encoding.UTF8.GetBytes(toEncrypt));
+            var decrypted = rsaParams.DecryptData(encrypted);
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("In Memory Key");
+            Console.WriteLine();
+            Console.WriteLine(" Original Text = " + toEncrypt);
+            Console.WriteLine(" Encrypted Text = " + Convert.ToBase64String(encrypted));
+            Console.WriteLine(" Decrypted Text = " + Encoding.Default.GetString(decrypted));
+
+            Console.ReadKey();
+        }
+    }
+}
